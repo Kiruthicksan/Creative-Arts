b59262da328073ad7e421263bd33c261e69428cd
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2026-01-16 12:41:37 +0530

    connect login endpoint to frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,16 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}));
 
 const PORT = process.env.PORT || 3000;
 
