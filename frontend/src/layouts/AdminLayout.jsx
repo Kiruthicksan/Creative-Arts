78a011a6606c0c924bcec07cda37d95414810714
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2026-01-25 19:52:14 +0530

    Start the AdminDashboare

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,16 +1,19 @@
 
 
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/public/Navbar';
+
+
+
 
 const AdminLayout = () => {
     return (
        <div>
-        <Navbar />
+       
          <main >
             {/* chidrens of parent route we give in app.jsx */}
             <Outlet />  
          </main>
+        
        </div>
     );
 };
