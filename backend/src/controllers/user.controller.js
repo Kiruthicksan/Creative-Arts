2c916fbaf7383302d0f7055d9192772e7f33871e
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2026-02-04 10:27:53 +0530

    create the users page

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,17 +1,29 @@
 import User from "../models/userSchema.js";
 
-export const getProfile = async (req , res) => {
-    const id = req.user.id;
-    const user = await User.findById(id);
-    if(!user){
-        return res.status(404).json({message: "User not found"});
-    }
-    res.status(200).json({user : {
-        _id: user._id,
-        userName: user.userName,
-        email: user.email,
-        role: user.role,
-        createdAt: user.createdAt
-    }});
-}
+export const getProfile = async (req, res) => {
+  const id = req.user.id;
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json({
+    user: {
+      _id: user._id,
+      userName: user.userName,
+      email: user.email,
+      role: user.role,
+      createdAt: user.createdAt,
+    },
+  });
 
+ 
+};
+
+ export const getAllUsers = async (req, res) => {
+    try {
+      const users = await User.find().sort({ createdAt: -1 });
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,10 @@
-import express from "express"
-import { getProfile } from "../controllers/user.controller.js";
+import express from "express";
+import { getProfile, getAllUsers } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/authMiddleWare.js";
 
 const router = express.Router();
 
 router.get("/profile", authMiddleware, getProfile);
+router.get("/all", getAllUsers);
 
 export default router;
